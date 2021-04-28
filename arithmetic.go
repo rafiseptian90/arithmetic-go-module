1639edc947869adf9ab4e7169a975691c8f2a97a
Rafi Septian Hadi <rafiseptian90@gmail.com>
2021-04-28 12:18:24 +0700

    v1

---
+++ arithmetic.go
@@ -0,0 +1,17 @@
+package arithmetic_go_module
+
+func Sum(number1, number2 int) int {
+	return number1 + number2
+}
+
+func Sub(number1, number2 int) int {
+	return number1 - number2
+}
+
+func Time(number1, number2 int) int {
+	return number1 * number2
+}
+
+func Div(number1, number2 int) int {
+	return number1 / number2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rafiseptian90/arithmetic-go-module
+
+go 1.16
